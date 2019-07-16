10e641f2bf6c0b88343d9398620cd81bfdbc011d
Sam <lutherye@gmail.com>
2019-07-16 13:08:38 -0400

    leetcode 07/16/2019

---
+++ 771.jewels-and-stones.php
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=771 lang=php
+ *
+ * [771] Jewels and Stones
+ */
+class Solution {
+
+    /**
+     * @param String $J
+     * @param String $S
+     * @return Integer
+     */
+    function numJewelsInStones($J, $S) {
+        $counter = 0;
+        for($i = 0; $i < strlen($S); $i++) {
+            if(strpos($J, $S[$i]) !== false) {
+                $counter++;
+            }
+        }
+        return $counter;
+    }
+}
+
